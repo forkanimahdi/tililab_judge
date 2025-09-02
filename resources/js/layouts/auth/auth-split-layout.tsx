716af960fec3f0cda77ead7829e3372c38c964f4
forkanimahdi <forkanimahdi@gmail.com>
2025-09-02 22:00:23 +0100

    refactor: forgot password  design +  change  language to french

--- resources/js/layouts/auth/auth-split-layout.tsx
+++ resources/js/layouts/auth/auth-split-layout.tsx
@@ -2,6 +2,8 @@ import AppLogoIcon from '@/components/app-logo-icon';
 import { type SharedData } from '@/types';
 import { Link, usePage } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
+import Hero from "../../../../public/assets/images/tlbg.jpg"
+
 
 interface AuthLayoutProps {
     title?: string;
@@ -15,8 +17,12 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
         <div className="relative grid h-dvh flex-col items-center justify-center px-8 sm:px-0 lg:max-w-none lg:grid-cols-2 lg:px-0">
             <div className="bg-muted relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
                 <div className="absolute inset-0 bg-zinc-900" />
+                <div className="">
+                    <div className="absolute w-full h-full top-0 left-0 bg-gradient-to-b from-black/20 to-black/90 z-10"></div>
+                    <img className='absolute w-full h-full left-0 top-0 object-cover' src={Hero} alt="" />
+                </div>
                 <Link href={route('home')} className="relative z-20 flex items-center text-lg font-medium">
-                    <AppLogoIcon size={200}  />
+                    <AppLogoIcon size={200} />
                     {name}
                 </Link>
                 {quote && (
@@ -31,7 +37,7 @@ export default function AuthSplitLayout({ children, title, description }: PropsW
             <div className="w-full lg:p-8">
                 <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
                     <Link href={route('home')} className="relative z-20 flex items-center justify-center lg:hidden">
-                        <AppLogoIcon size={200}  />
+                        <AppLogoIcon size={200} />
                     </Link>
                     <div className="flex flex-col items-start gap-2 text-left sm:items-center sm:text-center">
                         <h1 className="text-xl font-medium">{title}</h1>
